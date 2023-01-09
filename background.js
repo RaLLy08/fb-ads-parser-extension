30f83ed86da2f9716b6299de6e20f8c967bc60c3
RaLLy08 <uytemataaero@mail.ru>
2023-01-09 11:49:01 +0400

    see more clicking

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 const CHAT_ID ='';
 const BOT_TOKEN = '';
-// const CHAT_ID ='';
-// const BOT_TOKEN = '';
+
+
 
 const sleep = async (ms) => await new Promise((res, rej) => setTimeout(res, ms));
 
@@ -35,10 +35,21 @@ const getTelegramMessageLink = (ads, chatId, botToken) => {
     return (images || []).map((img, i) => `[Image ${i + 1}](${img})`).join('\n')
   }
 
+  const formatSiteLinks = (links) => {
+    return (links || []).map((link, i) => {
+      const { href, text } = link;
+
+      if (text === '') return `[Other Link: ${i + 1}](${href})`
+
+      return `[${text}](${href})`
+    }).join('\n')
+  }
+
   ads.forEach(pAd => {
     message += `${pAd.title}\n\n`;
     message += `${formatImages(pAd.images)}\n\n`;
     message += `${pAd.description}\n\n`;
+    message += `${formatSiteLinks(pAd.links)}\n\n`;
   })
 
   message = encodeURIComponent(message)
@@ -47,11 +58,64 @@ const getTelegramMessageLink = (ads, chatId, botToken) => {
 }
 
 async function injectParseAdsScript() {
-  window.scrollTo(0, document.body.scrollHeight)
+  window.scrollTo(0, document.body.scrollHeight);
 
-  await new Promise((res, rej) => setTimeout(res, 700 + 100*Math.random()))
+  const waitTextContentChange = async (el) => {
+    const prevTextContent = el.textContent;
+    const maxAttempt = 10;
+    const repeatCheckMs = 100;
+    let attemptCount = 0;
+    
+    return new Promise((res, rej) => {
+        setTimeout(() => check(), 0)
+        
+        const interval = setInterval(
+            () => {
+                check()
+
+                attemptCount += 1;
+                
+            },
+            repeatCheckMs
+        );
+
+        const check = () => {
+            if (el.textContent !== prevTextContent) {
+                clearInterval(interval);
+                res(el.textContent);
+            }
+
+            if (attemptCount > maxAttempt) {
+                clearInterval(interval);
+                rej('waitTextContentChange: timeout')
+            }
+        }
+
+        
+    })
+  };
+
+  await new Promise((res, rej) => setTimeout(res, 700 + 100*Math.random()));
   
-  const parseImages = (container) => {
+  const lang = document.querySelector('html').lang || 'en';
+
+  const adKeyWords = {
+    ru:'Реклама',
+    uk:'Реклама',
+    en:'Sponsored',
+  };
+
+  const seeMoreWords = {
+    ru:'Eщё',
+    uk:'Показати більше',
+    en:'See more',
+  };
+
+  const seeMoreWord = seeMoreWords[lang];
+  const adHookKeyWord = adKeyWords[lang];
+
+
+  const parseImageSrcs = (container) => {
     const images = container.querySelectorAll('img');
 
     const srcs = [];
@@ -67,35 +131,52 @@ async function injectParseAdsScript() {
     });
 
     return srcs;
-  }
+  };
 
-  const parseContainer = (container) => {
-    let images = [];
-    let description = '';
-    let title = '';
+  const parseDescription = (container) =>  container.querySelector(`div[data-ad-preview]`);
 
-    if (container) {
-      images = parseImages(container);
-      description = container.querySelector(`div[data-ad-preview]`).textContent;
-      title = container.querySelector('strong').textContent;
-    }
+  const parseTitle = (container) => container.querySelector('strong');
 
+  const parseThirdPartyLinks = (container) => {
+    const links = container.querySelectorAll('a');
+
+    return Array.from(links).map(link => {
+      const href = link.getAttribute('href');
+      const text = link.textContent.trim();
+
+      return { href, text };
+    }).filter(({ href })=> {
+      if (!href || !href.trim()) return false
+      
+      const isFbLink = new RegExp('ads/about').test(href);
+      
+      return !isFbLink
+    }).sort((a, b) => b.text.length - a.text.length)
+
+  };
+
+  const parseContainerData = (container) => {
     return {
-      images,
-      description,
-      title,
+      images: parseImageSrcs(container),
+      description: parseDescription(container).textContent,
+      title: parseTitle(container).textContent,
+      links: parseThirdPartyLinks(container),
     }
+  };
+
+  const parseSeeMoreButton = (container) => {
+    return Array.from(container.querySelectorAll('div[role="button"]')).find(el => el.textContent.includes(seeMoreWord))
   }
 
   const isAdContainer = (possibleContainer) => {
     if (!possibleContainer.contains(possibleContainer.querySelector(`div[data-ad-preview]`))) return false;
 
     return true
-  }
+  };
 
-  const data = [];
 
-  const adsContainerHooks = document.querySelectorAll(`a[aria-label="Реклама"]`) || []
+  const adsContainerHooks = document.querySelectorAll(`a[aria-label="${adHookKeyWord}"]`) || []
+  const adContainers = [];
 
   adsContainerHooks.forEach(adHook => {
     const container = adHook.closest('div[class=""]')
@@ -103,7 +184,28 @@ async function injectParseAdsScript() {
     if (!container) return;
     if (!isAdContainer(container)) return;
 
-    data.push(parseContainer(container));
+    adContainers.push(container);
+  });
+
+  for (const container of adContainers) {
+    const seeMoreButton = parseSeeMoreButton(container);
+
+    if (!seeMoreButton) continue;
+
+    const descriptionBlock = parseDescription(container);
+
+    seeMoreButton.click();
+
+    await waitTextContentChange(descriptionBlock);
+  };
+
+
+  const data = [];
+
+  adContainers.forEach(adContainer => {
+    const parsed = parseContainerData(adContainer);
+
+    data.push(parsed);
   })
   
   return {
@@ -139,12 +241,18 @@ chrome.storage.onChanged.addListener(
 
         const loop = async () => {
           const tabStateView = (await chrome.storage.sync.get(String(tabId)))[tabId];
-          const tabState = (await chrome.storage.local.get(String(tabId)))[tabId];
+          let tabState = (await chrome.storage.local.get(String(tabId)))[tabId];
+
+          if (!tabState.tabId) {
+            tabState = {
+              ...initialTabState,
+              tabId,
+            }
+          }
 
           if (!tabStateView.activateButton) return;
 
           const [ tabData ] = await parseAds(Number(tabId));
-
           const newAds = getDiff(tabData.result.data, tabState.ads)
 
           if (newAds.length) {
